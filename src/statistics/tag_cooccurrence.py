46dad0ef29258267f152f44f59faf5ecd1514e34
Asier Gutiérrez-Fandiño <asierguti96@gmail.com>
2022-03-03 11:56:05 +0100

    checkpoint

---
+++ src/statistics/tag_cooccurrence.py
@@ -0,0 +1,20 @@
+import json
+from urllib.parse import urlparse
+from collections import Counter
+from itertools import chain
+
+if __name__ == '__main__':
+    with open('../data/meneame_controversy_sampled.json', 'r') as fin:
+        data = list(map(json.loads, fin.readlines()))
+
+    data = [(d['controversy'], d['tags']) for d in data]
+    data = filter(lambda x: x[0], data)
+    data = [d[1] for d in data]
+    data = list(chain.from_iterable(data))
+    data = Counter(data)
+    data = data.items()
+    data = sorted(data, key=lambda x: x[1], reverse=True)
+    with open('../output/tag_coocurrence.tsv', 'w', encoding='utf-8') as fout:
+        fout.write('tag\tcount\n')
+        for d in data[:20]:
+            fout.write(f'{d[0]}\t{d[1]}\n')

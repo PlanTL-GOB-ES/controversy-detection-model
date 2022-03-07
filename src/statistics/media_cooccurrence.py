b76e3448484418d2e35adf2112d05ca3e0891d49
Asier Gutiérrez-Fandiño <asierguti96@gmail.com>
2022-03-07 15:51:27 +0100

    checkpoint

--- src/statistics/media_cooccurrence.py
+++ src/statistics/media_cooccurrence.py
@@ -21,18 +21,17 @@ if __name__ == '__main__':
 
     data = Counter([urlparse(url).netloc for url in urls]).items()
     data = sorted(data, key=lambda x: x[1], reverse=True)
-    top20 = data[:20]
 
     data_sampled_all = [d['url_website'] for d in data_sampled_all]
     data_sampled_all = Counter([urlparse(url).netloc for url in data_sampled_all])
-    sampled_all_counts = [data_sampled_all[url[0]] for url in top20]
+    sampled_all_counts = [data_sampled_all[url[0]] for url in data]
     data_all = Counter([urlparse(url).netloc for url in data_all])
-    data_all_counts = [data_all[url[0]] for url in top20]
+    data_all_counts = [data_all[url[0]] for url in data]
     with open('../output/media_coocurrence.csv', 'w', encoding='utf-8') as fout:
         csv_writer = csv.writer(fout, delimiter=',',
                                 quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
         csv_writer.writerow(['Source', 'Controversy Occurrences', 'Total Occurrences Sampled', 'Total Occurrences'])
-        for (media, count), sampled_count, all_count in zip(top20, sampled_all_counts, data_all_counts):
+        for (media, count), sampled_count, all_count in zip(data, sampled_all_counts, data_all_counts):
             csv_writer.writerow([media, count, sampled_count, all_count])
 
 

89ac3ad44b66a2681188360844760ae1fb0b48df
Asier Gutiérrez-Fandiño <asierguti96@gmail.com>
2022-04-19 17:43:42 +0200

    checkpoint

--- src/statistics/shap_compute_all.py
+++ src/statistics/shap_compute_all.py
@@ -26,14 +26,26 @@ def compute_all(explainer, content):
                 text = item['title']
             texts.append(text)
     shap_values = explainer(texts[0:10])
+    # Save computations
+    with open(f'../output/shap_results_{"content" if content else "no_content"}.json', 'w', encoding='utf-8') as fout:
+        for text, shap_result in zip(texts[0:10], shap_values):
+            out = {
+                'text': text,
+                'shap_values': shap_result.values[:, 0].tolist(),
+                'shap_text': shap_result.data
+            }
+            fout.write(json.dumps(out))
+            fout.write('\n')
+
+    # Compute by word
     for words, value in zip(shap_values.data, shap_values.values):
-        #words = shap_values.data[0]
+        # words = shap_values.data[0]
         label_0 = value[:, 0]
 
         for word, value in zip(words, label_0):
             data_0[word] = data_0[word] + value
 
-    with open(f'../output/label_0_{"content" if content else "no_content"}.json', 'w', encoding='utf-8') as fout:
+    with open(f'../output/shap_words_{"content" if content else "no_content"}.json', 'w', encoding='utf-8') as fout:
         json.dump(data_0, fout)
 
     print("Label 0")

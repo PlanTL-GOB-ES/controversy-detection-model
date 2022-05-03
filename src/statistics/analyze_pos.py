bf48c2580fa4679d2d0ed4ca8c343f3d177f52b2
BlancaCalvo <bcalvo.bsc@gmail.com>
2022-05-03 13:34:19 +0200

    analysis table

--- src/statistics/analyze_pos.py
+++ src/statistics/analyze_pos.py
@@ -10,8 +10,14 @@ if __name__ == '__main__':
     df_sum = df_group['value'].sum()
 
     for unique_entity in unique_entities:
-        print(unique_entity)
-        print("TOP 10")
-        print(df_sum[unique_entity].nlargest(10))
-        print("LAST 10")
-        print(df_sum[unique_entity].nsmallest(10))
+        print("\hline")
+        pos_selection = df_sum[unique_entity].nlargest(10)
+        neg_selection = df_sum[unique_entity].nsmallest(10)
+        pos = list(zip(pos_selection.index, pos_selection))
+        neg = list(zip(neg_selection.index, neg_selection))
+        for i, w in enumerate(pos):
+            if i == 4:
+                print(unique_entity, " & ", w[0], " & & ", f'{w[1]:.2f}', " & ", neg[i][0], " & & ", f'{neg[i][1]:.2f}', " \\\ ")
+            else:
+                print( " & ", w[0], " & & ", f'{w[1]:.2f}', " & ", neg[i][0], " & & ", f'{neg[i][1]:.2f}',
+                      " \\\ ")
